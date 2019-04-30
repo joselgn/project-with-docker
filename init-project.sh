35cd76c15964ac9a5012a68802f3b55b3d18d021
josefilho <joselgn@gmail.com>
2019-04-30 17:45:34 -0300

    [UPDATE] Ajustando scripts Linux & Removendo /t do docker-compose.yml

--- init-project.sh
+++ init-project.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+#Limpando bash
 clear
 
+#Verificando o Package Manager do SO
 if [ -f "/etc/redhat-release" ];then
    PKG_MANAGER=yum
 fi
@@ -17,16 +19,36 @@ if [ -f "/etc/debian_version" ];then
   PKG_MANAGER=apt-get
 fi
 
-
+#Atualizando o Packge Manager
 $PKG_MANAGER -y update;
+#Instalando dependências necessárias
 $PKG_MANAGER -y install git wget curl;
+#Instalando o Docker
 curl -fsSL https://get.docker.com | sh;
+#Instalando o docker-compose
 curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose;
 chmod +x /usr/local/bin/docker-compose;
 docker-compose --version;
+#Criando pasta de projeto para a aplicação
+cd /home/;
+#Clonando o Projeto
+git clone https://github.com/joselgn/project-with-docker.git project;
+cd project;
+#DOCKER ========= 
+#Criando a imagem da aplicação
+cd docker;
+docker build -t lojavirtual-img .;
+#Executando o docker-compose
+docker-compose up -d;
+#Configurando aplicação
+cd codigo;
+php composer.phar install;
+#Montando a estrutura do BD
+docker exec -ti lojavirtual-docker bash;
+php artisan migrate;
 
-mkdir /home/project; 
-cd /home/project; 
-git clone https://github.com/joselgn/ApiRestfullLaravel.git .
+
+
+
 
 
